cd3a779bbf0c4987d567e1785630dd91cf331ed4
krish rathi <148011352+krishrathi1@users.noreply.github.com>
2025-02-16 16:00:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript for interactive elements can be added here
+document.querySelector('.follow-btn').addEventListener('click', () => {
+    alert('Followed!');
+});
+
+document.querySelector('.message-btn').addEventListener('click', () => {
+    alert('Message sent!');
+});
